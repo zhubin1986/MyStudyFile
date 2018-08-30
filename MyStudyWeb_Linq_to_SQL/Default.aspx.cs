87eaeafc44d039faf45176c2a80838ee61500df5
zhubin1986 <869211741@qq.com>
2018-08-31 00:02:13 +0800

    初始化

---
+++ MyStudyWeb_Linq_to_SQL/Default.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web.DynamicData;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) {
+        System.Collections.IList visibleTables = ASP.global_asax.DefaultModel.VisibleTables;
+        if (visibleTables.Count == 0) {
+            throw new InvalidOperationException("没有可访问的表。请确保至少在 Global.asax 中注册了一个数据模型并启用了支架，或者实现自定义页。");
+        }
+        Menu1.DataSource = visibleTables;
+        Menu1.DataBind();
+    }
+
+}
