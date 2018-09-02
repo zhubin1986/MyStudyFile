cbd7d796d2ba7b12387ebff677a54e1c9ad0fc82
zhubin1986 <869211741@qq.com>
2018-09-02 22:13:03 +0800

    20180902

---
+++ CodingConversion/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CodingConversion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Encoding big5 = Encoding.GetEncoding("big5");
+            Encoding gb2312 = Encoding.GetEncoding("gb2312");
+            byte[] gb2312Ori = gb2312.GetBytes(textBox1.Text);
+            byte[] big5New = Encoding.Convert(gb2312, big5, gb2312Ori);
+            label1.Text = gb2312.GetString(big5New);
+        }
+    }
+}
---
+++ CodingConversion/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CodingConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
